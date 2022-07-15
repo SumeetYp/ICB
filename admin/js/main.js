22af12c84fb676a30276fc26c991fca531144e4b
Harshal141 <harshalmukundapatil@gmail.com>
2022-07-15 14:24:56 +0530

    Event Page Final

--- admin/js/main.js
+++ admin/js/main.js
@@ -5,7 +5,7 @@ let pastButton = document.querySelector('.pastButton');
 let addEvent = document.querySelector('.addEvent');
 
 function active(act,deact,display){
-    act.style.cssText += 'border-top-left-radius: 10px;border-top-right-radius: 10px;background-color: #D9D9D9;';
+    act.style.cssText += 'border-radius:0;border-top-left-radius: 10px;border-top-right-radius: 10px;background-color: #D9D9D9;';
     deact.style.cssText += 'border-radius: 0px;background-color: #fff;';
     addEvent.style.display=display;
 }
