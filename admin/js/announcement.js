61bd921ff663d98b28a44f79b99d1eb2e0f63677
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2022-07-29 19:52:16 +0530

    Fix in the header of admin/homepage

--- admin/js/announcement.js
+++ admin/js/announcement.js
@@ -21,23 +21,3 @@ const displayAnnouncements = async () => {
 };
 
 displayAnnouncements();
-
-const sideBar = document.querySelector(".sideBar");
-const menu = document.querySelector(".menu");
-const cross = document.querySelector(".cross");
-
-sideBar.classList.add("hideSideBar");
-
-menu.addEventListener("click", (e) => {
-  sideBar.classList.remove("hideSideBar");
-  sideBar.classList.add("displaySideBar");
-  setTimeout(() => {
-    cross.style.display = "flex";
-  }, 500);
-});
-
-cross.addEventListener("click", (e) => {
-  sideBar.classList.add("hideSideBar");
-  sideBar.classList.remove("displaySideBar");
-  cross.style.display = "none";
-});
