1b1b0e1e3ae81ebc7bf3502124f66727cf277062
MandeepDalavi <mandeepdalavi@gmail.com>
2022-08-09 13:49:13 +0530

    login support done with error page

--- login.php
+++ login.php
@@ -1,45 +1,32 @@
 <?php
+/* User login process, checks if user exists and password is correct */
 
-?>
-<!DOCTYPE html>
-<html lang="en-IN">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- favicon -->
-    <link rel="apple-touch-icon" sizes="180x180" href="img/favicon/apple-touch-icon.png">
-    <link rel="icon" type="image/png" sizes="32x32" href="img/favicon/favicon-32x32.png">
-    <link rel="icon" type="image/png" sizes="16x16" href="img/favicon/favicon-16x16.png">
-    <link rel="manifest" href="img/favicon/site.webmanifest">
-    <!-- font awesome -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    <!-- main css -->
-    <link rel="stylesheet" type="text/css" href="./css/login.css">
-    <title>Login | Member</title>
-</head>
-<body>
-    <div class="container">
-        <!-- <p class="login-text">Login with Social Account</p>
-        <div class="login-social">
-            <a href="#" class="facebook"><i class="fa fa-facebook"></i>Facebook</a>
-            <a href="#" class="twitter"><i class="fa fa-twitter"></i>Twitter</a>
-            <a href="#" class="google-plus"><i class="fa fa-google-plus"></i>Google+</a>
-            <a href="#" class="linkedin"><i class="fa fa-linkedin"></i>Linkedin</a>
-        </div> -->
-        <form action="" method="POST" class="login-email">
-            <p class="login-text">Login with Email</p>
-            <div class="input-group">
-                <input type="email" placeholder="Email" name="email" required>
-            </div>
-            <div class="input-group">
-                <input type="password" placeholder="Password" name="password" required>
-            </div>
-            <div class="input-group">
-                <button class="btn" name="submit">Login</button>
-            </div>
-            <p class="login-register-text">Don't have an account? <a href="https://bondsocially.org/contact.html">Contact Team</a></p>
-        </form>
-    </div>
-</body>
-</html>
+// Escape email to protect against SQL injections
+$email = $mysqli->escape_string($_POST['email']);
+$result = $mysqli->query("SELECT * FROM users WHERE email='$email' and (type='member' or type='student') ");
+
+if ( $result->num_rows == 0 ){ // User doesn't exist
+    $_SESSION['message'] = "User with that email doesn't exist! or is not a Member or Student";
+    header("location: error.php");
+}
+else { // User exists
+    $user = $result->fetch_assoc();
+
+    if ( password_verify($_POST['password'], $user['password']) ) {
+        
+        $_SESSION['email'] = $user['email'];
+        $_SESSION['username'] = $user['username'];
+        $_SESSION['type'] = $user['type'];
+        $_SESSION['first_name'] = $user['first_name'];
+        $_SESSION['last_name'] = $user['last_name'];
+        
+        // This is how we'll know the user is logged in
+        $_SESSION['logged_in'] = true;
+
+        header("location: index.php");
+    }
+    else {
+        $_SESSION['message'] = "You have entered wrong password, try again!";
+        header("location: error.php");
+    }
+}
