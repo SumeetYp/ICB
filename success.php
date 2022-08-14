c3f6624e6968a266cc6ccddab91b28473d9578cd
MandeepDalavi <mandeepdalavi@gmail.com>
2022-08-14 23:35:58 +0530

    added error and success page

--- success.php
+++ success.php
@@ -1,27 +1,27 @@
-<!-- <?php
-/* Displays all error messages */
+<?php
+/* Displays all successful messages */
 session_start();
-?> -->
+?>
 <!DOCTYPE html>
 <html>
 <head>
-  <title>Error</title>
+  <title>Success</title>
   <link href='http://fonts.googleapis.com/css?family=Titillium+Web:400,300,600' rel='stylesheet' type='text/css'>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css">
   <link rel="stylesheet" href="./css/error.css">
 </head>
 <body>
 <div class="form">
-    <h1>Error</h1>
+    <h1><?= 'Success'; ?></h1>
     <p>
     <?php 
-    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ): 
+    if( isset($_SESSION['message']) AND !empty($_SESSION['message']) ):
         echo $_SESSION['message'];    
     else:
-        header( "location: index_login.php" );
+        header( "location: login_index.php" );
     endif;
     ?>
-    </p>     
+    </p>
     <a href="index_login.php"><button class="button button-block"/>Home</button></a>
 </div>
 </body>
