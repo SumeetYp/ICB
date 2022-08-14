d40d789ddc9761851f4de2fcb3bcc008dbdaf787
MandeepDalavi <mandeepdalavi@gmail.com>
2022-08-14 22:14:31 +0530

    admin announcement backend added

--- admin/database/addAnnouncement.php
+++ admin/database/addAnnouncement.php
@@ -1,10 +1,8 @@
 <?php
     include('../config.php');
-    $expiryDate = $_POST['endDate'];
+    $expiryDate = $mysqli->escape_string($_POST['endDate']);
     $noticeDate = date("Y-m-d");
-    $content = $_POST['announcement'];
-    $sql = "INSERT INTO announcements (noticeDate, expiryDate, content) VALUES ('$noticeDate', '$expiryDate', '$content')";
-    $resultAddAnnouncement = mysqli_query($mysqli, $sql) or die ("SQL Failed");
-    mysqli_close($mysqli);
-    header("Location: ../index.php");
+    $content = $mysqli->escape_string($_POST['announcement']);
+    $mysqli->query("INSERT INTO announcements (noticeDate, expiryDate, content) VALUES ('$noticeDate', '$expiryDate', '$content')");
+    header("location: ./index.php");
 ?>
