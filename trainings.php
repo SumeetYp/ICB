a346c3cd0ffdd41a1b2225a1699c017c691bb9ce
Chirag Gupta <91630704+CHIRAG-GUPTA-987@users.noreply.github.com>
2022-08-15 17:51:32 +0530

    Readded security feature in training file

--- trainings.php
+++ trainings.php
@@ -2,9 +2,9 @@
     if (session_status() == PHP_SESSION_NONE) {
         session_start();
     }
-    // if (!(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true)){
-    //     header("Location: ./index_login.php");
-    // }
+    if (!(isset($_SESSION['logged_in']) && $_SESSION['logged_in']==true)){
+        header("Location: ./index_login.php");
+    }
     
     include './config.php';
 
