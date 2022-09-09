12fca8180dae600210526a62da7b7fdca7f1b762
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 15:06:30 +0530

    basic structure changed

--- donate.php
+++ donate.php
@@ -8,7 +8,7 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
+  <head>
     <meta charset="UTF-8" />
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
@@ -17,11 +17,11 @@
     <!-- <link rel="stylesheet" href="./css/main.css" /> -->
     <link rel="stylesheet" href="./css/header.css" />
     <link rel="stylesheet" href="./css/utils.css">
-</head>
-<body>
+  </head>
+  <body>
     <!-- Navigation Bar -->
     <?php
-        include './header.php';
+      include './header.php';
     ?>
     <script src="./js/sideBar.js"></script>
 
@@ -31,27 +31,27 @@
       <a href="https://rzp.io/l/bO34YmdFS" target="_blank">
         <div class="first-donate">
           <div class="align1">
-                <div class="donate">
+            <div class="donate">
               <h2>Donate</h2>
-                </div>
-                <div class="d-donate">
-              <h3>without 80G</h3>
-                </div>
             </div>
+            <div class="d-donate">
+              <h3>without 80G</h3>
+            </div>
+          </div>
         </div>
       </a>
       <a href="https://rzp.io/l/oaTOLRiB" target="_blank">
         <div class="second-donate">
           <div class="align2">
-                <div class="donate">
+            <div class="donate">
               <h2>Donate</h2>
-                </div>
-                <div class="d-donate">
-              <h3>with 80G</h3>
-                </div>
             </div>
+            <div class="d-donate">
+              <h3>with 80G</h3>
+            </div>
+          </div>
         </div>
       </a>
     </div>
-</body>
+  </body>
 </html>
