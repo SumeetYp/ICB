52db5dd158a52d331521d5dd87c3f31d71b61928
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 14:49:14 +0530

    changed links

--- logout.php
+++ logout.php
@@ -20,7 +20,7 @@ session_destroy();
               
           <p><?= 'You have been logged out!'; ?></p>
           
-          <a href="index_login.php"><button class="button button-block"/>Home</button></a>
+          <a href="index.php"><button class="button button-block"/>Home</button></a>
 
     </div>
 </body>
