3c2c70ae423d0f8c9f0923151b942eccb63fb5a3
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 14:39:20 +0530

    removed admin folder to reduce code redundancy

--- admin/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-    /* Database connection settings */
-    $host = 'localhost';
-    $user = 'root';
-    $pass = '';
-    $db = 'bs_portal';
-    $mysqli = new mysqli($host, $user, $pass, $db) or die($mysqli->error);
--- admin/database/addAnnouncement.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include('../config.php');
-    $expiryDate = $mysqli->escape_string($_POST['endDate']);
-    $noticeDate = date("Y-m-d");
-    $content = $mysqli->escape_string($_POST['announcement']);
-    $mysqli->query("INSERT INTO announcements (noticeDate, expiryDate, content) VALUES ('$noticeDate', '$expiryDate', '$content')");
-    header("location: ./index.php");
-?>
