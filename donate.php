1fa504299daf422e4d34ea0a456d578ca9d7472e
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 14:36:42 +0530

    basic structure changed

--- donate.php
+++ donate.php
@@ -8,56 +8,50 @@
 ?>
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta charset="UTF-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Document</title>
-    <link rel="stylesheet" href="./css/header.css">
-    <link rel="stylesheet" href="./css/main.css">
-    <link rel="stylesheet" href="./css/donate.css">
+    <link rel="stylesheet" href="./css/donate.css" />
+    <!-- <link rel="stylesheet" href="./css/main.css" /> -->
+    <link rel="stylesheet" href="./css/header.css" />
     <link rel="stylesheet" href="./css/utils.css">
 </head>
-
 <body>
     <!-- Navigation Bar -->
     <?php
         include './header.php';
     ?>
+    <script src="./js/sideBar.js"></script>
 
     <!-- donate page  -->
+    <p class="heading">Donate</p>
     <div class="container">
+      <a href="https://rzp.io/l/bO34YmdFS" target="_blank">
         <div class="first-donate">
-            <div class="another-div">
+          <div class="align1">
                 <div class="donate">
-                    <h2>
-                        Donate
-                    </h2>
+              <h2>Donate</h2>
                 </div>
                 <div class="d-donate">
-                    <h3>
-                        without 80G
-                    </h3>
+              <h3>without 80G</h3>
                 </div>
             </div>
         </div>
+      </a>
+      <a href="https://rzp.io/l/oaTOLRiB" target="_blank">
         <div class="second-donate">
-            <div class="another-div">
+          <div class="align2">
                 <div class="donate">
-                    <h2>
-                        Donate
-                    </h2>
+              <h2>Donate</h2>
                 </div>
                 <div class="d-donate">
-                    <h3>
-                        without 80G
-                    </h3>
+              <h3>with 80G</h3>
                 </div>
             </div>
         </div>
+      </a>
     </div>
-    <script src="./js/sideBar.js"></script>
 </body>
-
 </html>
