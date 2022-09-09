99542f7ffd70fa0d47745cbacb44f9a17c129358
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 14:48:01 +0530

    made header dynamic with respect to userType

--- header.php
+++ header.php
@@ -3,12 +3,64 @@
     $borderColor = NULL;
     switch($_SESSION['type']){
         case "admin": $borderColor= '#0ED678'; 
-                      $checkSrc= './images/check 1admin.png';
+                      $checkSrc= './images/checkadmin.png';
+                      $menu =  "<a href='./home.php'>Home</a>
+                                <a href='./stats.php'>Stats</a>
+                                <a href='./eventsManage.php'>Event Manager</a>
+                                <a href='./announcement.php'>Announcement</a>
+                                <a href='./profile.php'>Profile</a>
+                                <a href='./trainings.php'>My Training</a>
+                                <a href='./events.php'>My Events</a>
+                                <a href='./differenceIMade.php'>Difference I Made</a>
+                                <a href='./shareMyStory.php'>Share My Story</a>
+                                <a href='./donate.php'>Donate</a>
+                                <a href='./addMarshalls.php'>Add a Marshal</a>
+                                <a href='./settings.php'>Settings & Support</a>
+                                <a href='./logout.php'>Logout</a>";
                       break;
+        case "core-team": $borderColor= '#FC8955';
+                        $checkSrc= './images/shield core-team.png';
+                        $menu =  "<a href='./home.php'>Home</a>
+                                    <a href='./eventsManage.php'>Event Manager</a>
+                                    <a href='./profile.php'>Profile</a>
+                                    <a href='./trainings.php'>My Training</a>
+                                    <a href='./events.php'>My Events</a>
+                                    <a href='./differenceIMade.php'>Difference I Made</a>
+                                    <a href='./shareMyStory.php'>Share My Story</a>
+                                    <a href='./donate.php'>Donate</a>
+                                    <a href='./addMarshalls.php'>Add a Marshal</a>
+                                    <a href='./settings.php'>Settings & Support</a>
+                                    <a href='./coreTeam.php'>Contact Team</a>
+                                    <a href='./alert.php'>Send an Alert</a>
+                                    <a href='./logout.php'>Logout</a>";
+                        break;
         case "member": $borderColor= '#2196F3';
                        $checkSrc= './images/memberProfile.svg';
+                       $menu = "<a href='./home.php'>Home</a>
+                                <a href='./profile.php'>Profile</a>
+                                <a href='./trainings.php'>My Training</a>
+                                <a href='./events.php'>My Events</a>
+                                <a href='./differenceIMade.php'>Difference I Made</a>
+                                <a href='./shareMyStory.php'>Share My Story</a>
+                                <a href='./donate.php'>Donate</a>
+                                <a href='./settings.php'>Settings & Support</a>
+                                <a href='./coreTeam.php'>Contact Team</a>
+                                <a href='./alert.php'>Send an Alert</a>
+                                <a href='./logout.php'>Logout</a>";
                        break;
-        case "volunteer": $borderColor= '#FFBE00';
+        case "student": $borderColor= '#FFC4C4';
+                        $menu ="<a href='./home.php'>Home</a>
+                                <a href='./profile.php'>Profile</a>
+                                <a href='./trainings.php'>My Training</a>
+                                <a href='./events.php'>My Events</a>
+                                <a href='./differenceIMade.php'>Difference I Made</a>
+                                <a href='./shareMyStory.php'>Share My Story</a>
+                                <a href='./donate.php'>Donate</a>
+                                <a href='./settings.php'>Settings & Support</a>
+                                <a href='./coreTeam.php'>Contact Team</a>
+                                <a href='./alert.php'>Send an Alert</a>
+                                <a href='./logout.php'>Logout</a>";
+                       break;
     }
     $display = '';
     if($checkSrc == NULL){
@@ -22,25 +74,14 @@
             </div>  
             <div class='userName'>" . $_SESSION['username'] . "</div>" . "\n" .
                 "<div class='profilePicture'>" . "\n" .
-                "<img class='profPic' src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTfAcQBipWyY0qIXJvbIEOnGmkvcXJBKA-3Yg&usqp=CAU' style='border-color: " . $borderColor . ";' alt=''>" . "\n" .
+                "<img class='profPic' src='".$_SESSION['profile']."' style='border-color: " . $borderColor . ";' alt=''>" . "\n" .
                 "<img class='check " . $display . "' src='" . $checkSrc . "' alt=''>" . "\n" .
             "</div>
         </nav>" .
         "<div class='sideBar hideSideBar'>
             <div class='sideItems'>
                 <ul>
-                    <a href='./index.php'>Home</a>
-                    <a href='./profile.php'>Profile</a>
-                    <a href='./trainings.php'>My Training</a>
-                    <a href='./events.php'>My Events</a>
-                    <a href='./donate.php'>Donate</a>
-                    <a href='./differenceIMade.php'>Difference I Made</a>
-                    <a href='./shareMyStory.php'>Share My Story</a>
-                    <a href='./addMarshalls.php'>Add a Marshal</a>
-                    <a href='./settings.php'>Settings & Support</a>
-                    <a href='./coreTeam.php'>Contact Team</a>
-                    <a href='./alert.php'>Send an Alert</a>
-                    <a href='./logout.php'>Logout</a>
+                    ".$menu."
                 </ul>
                 <div class='cross'>
                     <img src='./images/cross.png' alt=''>
