1876139ca7e5bf419621127a72bda87afb495974
MandeepDalavi <mandeepdalavi@gmail.com>
2022-09-09 15:03:58 +0530

    updated css and js files

--- js/sideBar.js
+++ js/sideBar.js
@@ -1,12 +1,14 @@
 const sideBar = document.querySelector('.sideBar');
 const hamBurger = document.querySelector('.hamBurger');
 const cross = document.querySelector('.cross');
+const body = document.getElementsByTagName('body')[0];
 
 sideBar.classList.add('hideSideBar');
 
 hamBurger.addEventListener('click', e=>{
     sideBar.classList.remove('hideSideBar');
     sideBar.classList.add('displaySideBar');
+    body.classList.add('fixed-position');
     setTimeout(()=>{
         cross.style.display = 'flex';
     }, 500);
@@ -15,5 +17,6 @@ hamBurger.addEventListener('click', e=>{
 cross.addEventListener('click', e=>{
     sideBar.classList.add('hideSideBar');
     sideBar.classList.remove('displaySideBar');
+    body.classList.remove('fixed-position');
     cross.style.display = 'none';
 })
