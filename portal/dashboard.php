b1f99a96d0c0b2fcd343308bca6f704c65afc37b
Sumeetyp <sumeet.pandhare@aissmsioit.org>
2023-09-13 14:38:54 +0530

    Associate Dashboard Created

--- portal/dashboard.php
+++ portal/dashboard.php
@@ -38,30 +38,59 @@ $searchOn = ''
   <header>
   <div class="welcome">
   <div class="welcome-strip"></div>
+  <div class="profile-icon">
+      <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+        <!-- SVG path for your profile icon -->
+      </svg>
+    </div>
+  <div class="welcome-text">Welcome to ICB</div>
+  <div class="uid-text">UID: 16426790</div>
+  <div class="dashboard-text">Dashboard</div>
   <div class="welcome-container">
     <a href="#"></a>
     <a href="#"></a>
     <a href="#"></a>
     <a href="#"></a>
   </div>
-  <div class="card">
-  TOTAL SELLS
-  <a href="" target="_blank">Total sells</a>
+  <div class="cards-container">
+    <div class="card card-left">
+      <div class="card-content">
+        <span class="card-title">TOTAL SELLS</span>
+        <div class="card-icon">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+            <!-- SVG path for your icon -->
+          </svg>
+          <span class="card-text">Rs 1500</span>
+        </div>
+        <a href="#" class="card-link" target="_blank"></a>
+      </div>
+    </div>
+    <div class="card card-center">
+      <div class="card-content">
+        <span class="card-title">TOTAL EARNING</span>
+        <div class="card-icon">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+            <!-- SVG path for your icon -->
+          </svg>
+          <span class="card-text">Rs 1500</span>
+        </div>
+        <a href="#" class="card-link" target="_blank"></a>
+      </div>
+    </div>
+    <div class="card card-right">
+      <div class="card-content">
+        <span class="card-title">MY NETWORK</span>
+        <div class="card-icon">
+          <svg class="icon" xmlns="http://www.w3.org/2000/svg" width="30" height="30" viewBox="0 0 30 30">
+            <!-- SVG path for your icon -->
+          </svg>
+          <span class="card-text">40</span>
+        </div>
+        <a href="#" class="card-link" target="_blank"></a>
+      </div>
+    </div>
+  </div>
 </div>
-<div class="card">
-  TOTAL EARNING
-  <a href="" target="_blank">Total Earnings</a>
-</div>
-<div class="card">
-  MY NETWORK
-  <a href="" target="_blank">My Network</a>
-</div>
-</div>
-
-
-
-
-
 
 </body>
 
