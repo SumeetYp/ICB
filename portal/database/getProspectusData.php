fd5acd6b3921bc59de574ccbf7ef36329243284b
Akhil-A0402 <akhil.a.nair760@gmail.com>
2023-09-23 23:09:22 +0530

    Making product id avaialble

--- portal/database/getProspectusData.php
+++ portal/database/getProspectusData.php
@@ -12,7 +12,7 @@
 
     $id = $_SESSION['id'];
     if($id!=null){
-        $queryProspectus = "select order_table.order_id, product.name product_name, order_table.quantity, order_table.customer_id, customer.name customer_name, customer.whatsapp, customer.email, prospectus.status from order_table inner join product on order_table.product_id = product.id inner join customer on customer.id = order_table.customer_id inner join prospectus on prospectus.order_id = order_table.order_id WHERE order_table.associate_id =".$id.";";
+        $queryProspectus = "select product.id as product_id,order_table.order_id, product.name product_name, order_table.quantity, order_table.customer_id, customer.name customer_name, customer.whatsapp, customer.email, prospectus.status from order_table inner join product on order_table.product_id = product.id inner join customer on customer.id = order_table.customer_id inner join prospectus on prospectus.order_id = order_table.order_id WHERE order_table.associate_id =".$id.";";
         $resultProspectus = mysqli_query($mysqli, $queryProspectus) or die ("SQL Failed");
         $output = "{";
         while($row = mysqli_fetch_array($resultProspectus)){
@@ -21,7 +21,7 @@
             $status = $row['status']==1?'Pitching': ($row['status']==2?'Closed':'Cancelled');
             // echo $row['status'];
             // $output.="{'product_name':'".$row['product_name']."', 'quantity':".$row['quantity'].", 'customer_id':".$row['customer_id'].", 'customer_name':'".$row['customer_name']."', 'whatsapp':".$row['whatsapp'].", 'email':'".$row['email']."', 'status':'".$status."'},";
-            $output.='"'.$row["customer_id"].'":{"order_id":"'.$row['order_id'].'","product_name":"'.$row["product_name"].'", "quantity":'.$row["quantity"].', "customer_id":'.$row["customer_id"].', "customer_name":"'.$row["customer_name"].'", "whatsapp":'.$row["whatsapp"].', "email":"'.$row["email"].'", "status":"'.$status.'"},';
+            $output.='"'.$row["customer_id"].'":{"product_id":"'.$row["product_id"].'","order_id":"'.$row['order_id'].'","product_name":"'.$row["product_name"].'", "quantity":'.$row["quantity"].', "customer_id":'.$row["customer_id"].', "customer_name":"'.$row["customer_name"].'", "whatsapp":'.$row["whatsapp"].', "email":"'.$row["email"].'", "status":"'.$status.'"},';
         }
         $output=trim($output, ',').'}';
         echo $output;
