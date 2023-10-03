879b504a3a5cfb8b29adfc0454ada9ee4a57d73d
Akhil-A0402 <akhil.a.nair760@gmail.com>
2023-10-03 17:54:31 +0530

    dashbaord and header made responsive

--- portal/dashboard.php
+++ portal/dashboard.php
@@ -32,7 +32,7 @@ $searchOn = ''
   <script src="https://cdn.jsdelivr.net/npm/@splidejs/splide@3.1.9/dist/js/splide.min.js"></script>
 
 
-  <sty le>
+  <style>
     <?php include "./css/header.css" ?><?php include "./css/search.css" ?><?php include "./css/servicecard.css" ?>
   </style>
   <link rel="stylesheet" href="./css/bottomNav.css">
